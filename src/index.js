21cb4be426fbfb31f5abf78d4f1ffa81b5841614
kolanowskim <kolanowskim99@gmail.com>
2022-02-14 16:15:16 +0100

    get styles from index.js to index.tmp.html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { Loader } from "@googlemaps/js-api-loader";
 import { initMap } from "../functions/initMap";
-import style from "../styles/index.css";
+/* import style from "../styles/index.css"; */
 
 const loader = new Loader({
   apiKey: process.env.GOOGLE_API_KEY,
